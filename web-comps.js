3aabe155431120048aa76863c0541a4c8840d0a6
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-12-06 14:15:38 -0800

    Update web-comps.js

--- web-comps.js
+++ web-comps.js
@@ -3,7 +3,13 @@ customElements.define('rating-widget', class extends HTMLElement {
   constructor () {
     super();
 
-    this.innerHTML = '☆☆☆☆☆';
+    this.innerHTML = '';
+    this.stare = document.createElement('input');
+    this.appendChild(this.stars)
+    
+  }
+
+  updateStars(){
     
   }
 
