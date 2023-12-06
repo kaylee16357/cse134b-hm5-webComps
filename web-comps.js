b708ad718b70fbe04d93d7d7247a3b262eb2d73a
kaylee16357 <81766599+kaylee16357@users.noreply.github.com>
2023-12-06 14:19:44 -0800

    Update web-comps.js

--- web-comps.js
+++ web-comps.js
@@ -1,19 +1,1 @@
-customElements.define('rating-widget', class extends HTMLElement {
 
-  constructor () {
-    super();
-
-    this.innerHTML = '';
-    this.stare = document.createElement('input');
-    this.appendChild(this.stars)
-    
-  }
-
-  updateStars(){
-    
-  }
-
-
-
-
-}
